79d60990f6f6c8dcb0aeb7ba23d678fe862ec2d7
Thomas Depierre <depierre.thomas@gmail.com>
2022-06-25 16:36:23 +0200

    update docs for callbacks

--- src/hyper_register.erl
+++ src/hyper_register.erl
@@ -22,9 +22,6 @@
     bytes/2
 ]).
 
-%% @doc: Set the register to the given value, *only* if the value
-%% already stored is lower than the new value. The backend needs to
-%% ensure the register value is only allowed to increase.
 -callback set(
     Index :: integer(),
     Value :: integer(),
@@ -32,62 +29,41 @@
 ) ->
     hyper:registers().
 
-%% @doc: Creates a new instance of the backend. The return value of
-%% this function will be passed to all functions in this module.
 -callback new(P :: hyper:precision()) ->
     hyper:registers().
 
-%% @doc: Compact is always called before any attempt at reading (sum,
-%% zero count, etc) or merging. It is intended to give backends that
-%% buffer the writes a chance to flush the buffer before the registers
-%% are needed.
 -callback compact(hyper:registers()) ->
     hyper:registers().
 
-%% @doc: Merge any number of registers, used to calculate the
-%% union. For two register values at the same index, the max value
-%% must be in the resulting register.
 -callback max_merge([hyper:registers()]) ->
     hyper:registers().
 
-%% @doc: Same as max_merge/1 but used when we know only two filters
-%% are merged.
 -callback max_merge(
     hyper:registers(),
     hyper:registers()
 ) ->
     hyper:registers().
 
-%% @doc: Reduce the precision of the registers. Used for mixed-precision
-%% union by first reducing the precision to the lowest of all filters.
 -callback reduce_precision(
     hyper:precision(),
     hyper:registers()
 ) ->
     hyper:registers().
 
-%% @doc: Sum of 2^-R where R is the value in each register.
 -callback register_sum(hyper:registers()) ->
     float().
 
-%% @doc: A map with key the values possible for the register and values the
-%% number of register with that value.
 -callback register_histogram(hyper:registers()) -> map().
 
-%% @doc: Count of registers set to 0.
 -callback zero_count(hyper:registers()) ->
     integer().
 
-%% @doc: Encode and decode are called to convert the in-memory
-%% representation of the backend to the serialized format. Must return
-%% one binary where each register is encoded as an 8-bit integer.
 -callback encode_registers(hyper:registers()) ->
     binary().
 
 -callback decode_registers(binary(), hyper:precision()) ->
     hyper:registers().
 
-%% @doc: Size in bytes used to represent the registers in memory.
 -callback bytes(hyper:registers()) ->
     integer().
 
